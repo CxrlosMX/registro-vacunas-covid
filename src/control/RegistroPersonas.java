a3fcf926e2a2485448f1353a8cd660cbe9914f1a
CxrlosMX <LuisCRendon131@gmail.com>
2021-08-09 20:13:23 -0500

    Inicio clase registro personas

--- src/control/RegistroPersonas.java
+++ src/control/RegistroPersonas.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package control;
 
+import clasepadre.Persona;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,9 +16,21 @@ import java.util.List;
  * @Phone: 953-212-97-27
  * @Email: LuisCRendon131@gmail.com
  * @Date: 8/08/2021
- * 
+ *
  */
 public class RegistroPersonas {
+
+    private ArrayList<Persona> lista;
+    private int contador;
+
+    public RegistroPersonas() {
+        lista = new ArrayList<Persona>();
+        contador = -1;
+    }
     
+    //Método para verificar si nuestra lista esta vacia
+    public boolean estaVacia(){
+    return (this.contador==-1); //La lista esta vacia si el contador es igual a -1
+    }
 
 }
